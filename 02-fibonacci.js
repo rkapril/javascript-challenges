17c2ec23844545301a26304d3f96d51bd0508e53
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-10-28 20:36:34 +0800

    Update 02-fibonacci.js

--- 02-fibonacci.js
+++ 02-fibonacci.js
@@ -18,3 +18,22 @@ function fibonacciGenerator(n) {
 }
 
 console.log(fibonacciGenerator(0));
+
+// For Loop
+function fibonacciGenerator(n) {
+  output = [];
+
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else if (n > 2) {
+    output = [0, 1];
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+  return output;
+}
+
+console.log(fibonacciGenerator(3));
