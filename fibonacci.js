bbda7a92dabd941a61502ae5bb4cc2892a78200c
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-10-28 20:11:51 +0800

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+function fibonacciGenerator(n) {
+  output = [];
+
+  if (n === 1) {
+    output.push(0);
+  } else if (n === 2) {
+    output.push(0);
+    output.push(1);
+  } else if (n > 2) {
+    output.push(0);
+    output.push(1);
+    while (n !== output.length) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+  return output;
+}
+
+console.log(fibonacciGenerator(0));
